68a83c9f2fd2c1ec42b3233cb7f7e9e01cdef5f7
Ben J. Ward <ward9250@gmail.com>
2017-09-02 15:56:32 +0100

    Drop Mkdocs

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
 # Contributing
+
 [![Build Status](https://travis-ci.org/BioJulia/Contributing.svg?branch=master)](https://travis-ci.org/BioJulia/Contributing)
 
-**Contents:**
+The core rules and guidelines set out by the BioJulia community, for
+contributors AND its members and maintainers.
 
-- CONTRIBUTING.md: A general statement of contribution guidelines, for contributing to BioJulia repositories. It should be included with every BioJulia git(hub) repository, in the top-level directory of that repository.
-
-- docs/ : Documentation which generates the Contributing page of the website. Intended to mirror content of CONTRIBUTING.md and be
-          friendly to new users. If you update CONTRIBUTING.md or docs/, then please make sure they faithfully represent each other's              contents.
+To start a discussion for a rule or guideline addition, make a PR or issue. 
--- docs/make.jl
+++ docs/make.jl
@@ -1,10 +1,33 @@
 using Documenter
 
-custom_deps() = run(`pip install --user pygments mkdocs mkdocs-material`)
-makedocs()
+makedocs(
+    format = :html,
+    sitename = "Contributing to BioJulia",
+    pages = [
+        "Home" => "index.md",
+        "How do I...?" => [
+            "Contribute packages" => "help/package_submission.md",
+            "Join the BioJulia GitHub organisation" => "help/join_biojulia.md",
+            "Choose a branching model" => "help/branching_guide.md"
+        ],
+        "Policies and Guidelines" => [
+            "Community Organisation" => "policy/biojulia_organisation.md",
+            "Code of conduct" => "policy/contributor_covenant.md",
+            "Incident Response" => "policy/incident_response.md",
+            "Code Style" => "policy/code_style.md",
+            "Package Requirements" => "policy/package_requirements.md",
+            "Pull Requests" => "policy/pull_requests.md",
+            "Off-boarding" => "policy/off_boarding.md"
+        ]
+    ],
+    authors = "Ben J. Ward, The BioJulia organisation, and contributors."
+)
+
 deploydocs(
-           repo = "github.com/BioJulia/Contributing.git",
-           julia = "0.4",
-           osname = "linux",
-           deps = custom_deps
-           )
+    repo = "github.com/BioJulia/Contributing.git",
+    julia = "0.6",
+    osname = "linux",
+    target = "build",
+    deps = nothing,
+    make = nothing
+)
